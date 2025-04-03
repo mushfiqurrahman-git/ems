d8f27f3755587f53ae21f17144aea2064b38b88e
mushfiqurahman-git <mymensingh456@gmail.com>
2025-04-03 10:13:41 -0400

    user admid seperation

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Login from './components/Auth/Login'
 import { getLocalStorage, setLocalStorage } from './utils/localStorage'
+import { AuthContext } from './context/AuthProvider'
+import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
+import AdminDashboard from './components/Dashboard/AdminDashboard'
 
 
 const App = () => {
 
   const [user, setUser] = useState(null)
 
+  const authData = useContext(AuthContext)
+  console.log(authData.employees.find((e)=>e.email == e.email && e.password == password))
+
   const handleLogin = (email, password) => {
     if (email == 'admin@me.com' && password == '123') {
       setUser('admin')
-      console.log(user)
     }
     else if (email == 'user@me.com' && password == '123') {
       setUser('employee')
-      console.log(user)
     }
     else {
       alert("Invalid Credentials")
@@ -22,10 +26,13 @@ const App = () => {
 
   }
 
+  const data = useContext(AuthContext)
+  console.log(data)
+
   return (
     <>
       {!user ? <Login handleLogin={handleLogin} /> : ''}
-      {user == 'admin' ? <AdminDashboard /> : <EmplopyeeDashboard />}
+      {user == 'admin' ? <AdminDashboard /> :<EmployeeDashboard/>}
     </>
   )
 }
